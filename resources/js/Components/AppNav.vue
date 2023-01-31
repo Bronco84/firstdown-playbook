63ed71eb1bdd4221f48040316bef2cafc6d80bff
Brad McColey <bmccoley@southernrepro.com>
2023-01-31 11:43:33 -0600

    Fixes for Mobile

--- resources/js/Components/AppNav.vue
+++ resources/js/Components/AppNav.vue
@@ -2,7 +2,7 @@
     <Disclosure as="nav" class="bg-black z-20" v-slot="{ open }">
       <div class="4xl:mx-auto 4xl:max-w-7xl px-2 sm:px-8">
         <div class="relative flex items-center justify-between" style="height:73px">
-          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
             <!-- Mobile menu button-->
             <DisclosureButton class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span class="sr-only">Open main menu</span>

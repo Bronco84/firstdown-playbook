b9fba65f73e28a624ff6635609e89a54465b6b62
Brad McColey <bmccoley@southernrepro.com>
2023-01-31 11:46:27 -0600

    Update web.php
    Add products route

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,10 @@ Route::get('/pricing', function () {
     return Inertia::render('Pricing');
 })->name('pricing');
 
+Route::get('/products', function () {
+    return Inertia::render('Pricing');
+})->name('products');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');

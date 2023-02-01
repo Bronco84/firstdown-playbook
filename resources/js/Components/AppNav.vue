63d93dc45d40fcb43bec0bf7f43e62bb3d8b9b42
Brad McColey <bmccoley@southernrepro.com>
2023-01-31 19:24:19 -0600

    Update AppNav.vue

--- resources/js/Components/AppNav.vue
+++ resources/js/Components/AppNav.vue
@@ -96,7 +96,7 @@
         </div>
       </div>
       <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-        <DisclosurePanel class="sm:hidden">
+        <DisclosurePanel class="md:hidden">
             <div class="space-y-1 px-2 pt-2 pb-3">
                 <template v-for="item in navigation">
                     <Link :href="item.href">

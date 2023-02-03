f432d355cce2163b79a7078a50abbed7aef24148
Brad McColey <bmccoley@southernrepro.com>
2023-02-03 15:49:46 -0600

    Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="https://playbook-tech-assets.s3.amazonaws.com/firstdown/favicon.ico">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
